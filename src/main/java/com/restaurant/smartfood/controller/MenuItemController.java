57ad9e36873bcd512fdcf028c057f1cff400780e
Shai <Shaisisso1@gmail.com>
2022-09-30 15:39:53 +0300

    change logger and add messages

--- src/main/java/com/restaurant/smartfood/controller/MenuItemController.java
+++ src/main/java/com/restaurant/smartfood/controller/MenuItemController.java
@@ -4,6 +4,7 @@ package com.restaurant.smartfood.controller;
 import com.restaurant.smartfood.entities.MenuItem;
 import com.restaurant.smartfood.service.MenuItemService;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,7 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("/api/menu")
-@Log4j2
+@Slf4j
 public class MenuItemController {
 
 	@Autowired
@@ -21,12 +22,14 @@ public class MenuItemController {
 	
 	@PostMapping
 	public MenuItem addItem(@RequestBody MenuItem item) {
+		log.info("ADD Item");
 		return menuItemService.addItem(item);
 	}
 	
 	
 	@GetMapping
 	public List<MenuItem>  getMenu() {
+		log.warn("get menu");
 		return menuItemService.getMenu();
 	}
 	

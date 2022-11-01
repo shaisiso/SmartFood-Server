6029b5d436a3d51f16b9e70f50f5d738e479c90e
Shai <Shaisisso1@gmail.com>
2022-11-01 17:11:10 +0200

    Update Discount fields to be NotNull

--- src/main/java/com/restaurant/smartfood/entities/Discount.java
+++ src/main/java/com/restaurant/smartfood/entities/Discount.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -22,40 +23,62 @@ import java.util.Set;
 @Entity
 public class Discount {
     @Id
-    @Column(name = "discountID", nullable = false)
-    private Long discountID;
+    @Column(name = "discount_id", nullable = false)
+    private Long discountId;
 
+    @NotNull
+    @Column(nullable = false)
     private Boolean forMembersOnly;
 
     @FutureOrPresent
     @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull
+    @Column(nullable = false)
     private LocalDate startDate;
 
     @FutureOrPresent
     @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull
+    @Column(nullable = false)
     private LocalDate endDate;
 
     @ElementCollection
+    @NotNull
+    @Column(nullable = false)
     private Set<DayOfWeek> days;
+
 
     @FutureOrPresent
     @JsonFormat(pattern = "HH:mm")
+    @NotNull
+    @Column(nullable = false)
     private LocalTime startHour;
 
     @FutureOrPresent
     @JsonFormat(pattern = "HH:mm")
+    @NotNull
+    @Column(nullable = false)
     private LocalTime endHour;
 
     @Min(1)
+    @NotNull
+    @Column(nullable = false)
     private Integer ifYouOrder;
 
     @Min(1)
+    @NotNull
+    @Column(nullable = false)
     private Integer youGetDiscountFor;
 
     @Min(0)
     @Max(100)
+    @NotNull
+    @Column(nullable = false)
     private Integer percent;
 
     @ElementCollection
-    private  List<ItemCategory> discountCategories;
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    @Column(nullable = false)
+    private List<ItemCategory> categories;
 }

57e33758886b4b047e876808df78698bef84ac00
Shai <Shaisisso1@gmail.com>
2022-11-01 20:15:36 +0200

    add customer field to TakeAway

--- src/main/java/com/restaurant/smartfood/entities/TakeAway.java
+++ src/main/java/com/restaurant/smartfood/entities/TakeAway.java
@@ -4,14 +4,21 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @SuperBuilder
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
 @Entity
-@Table()
+@Table(name = "take_away")
 public class TakeAway extends Order {
+
+    @ManyToOne(optional = false)
+    @NotNull
+    private Customer customer;
 }

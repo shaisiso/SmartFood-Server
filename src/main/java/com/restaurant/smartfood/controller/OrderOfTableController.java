4b9bdb9445143bb80a6e785cb2968a91641a0a31
Shai <Shaisisso1@gmail.com>
2022-11-25 16:56:07 +0200

    comment unfinshed methods

--- src/main/java/com/restaurant/smartfood/controller/OrderOfTableController.java
+++ src/main/java/com/restaurant/smartfood/controller/OrderOfTableController.java
@@ -26,34 +26,34 @@ public class OrderOfTableController {
     public OrderOfTable updateOrderOfTable(@RequestBody @Valid OrderOfTable orderOfTable) {
         return orderOfTableService.updateOrderOfTable(orderOfTable);
     }
-
-    @DeleteMapping
-    public void deleteOrderOfTable(@RequestBody @Valid OrderOfTable orderOfTable) {
-        orderOfTableService.deleteOrderOfTable(orderOfTable);
-    }
-
-    @GetMapping("/{orderid}")
-    public OrderOfTable getOrderOfTableByOrderId(@PathVariable("orderid") Long orderId) {
-        return orderOfTableService.getOrderOfTableByOrderId(orderId);
-    }
-
-    @GetMapping("/active")
-    public List<OrderOfTable> getActiveOrdersOfTables() {
-        return orderOfTableService.getActiveOrdersOfTables();
-    }
-
-    @GetMapping("/dates/{startDate}/{endDate}")
-    public List<OrderOfTable> getOrdersOfTablesByDates(
-            @PathVariable("startDate") String startDate,
-            @PathVariable("endDate") String endDate) {
-        return OrderOfTableService.getOrdersOfTablesByDates(startDate, endDate);
-    }
-
-    @GetMapping("/status/{status}")
-    public List<OrderOfTable> getOrdersOfTablesByStatus(
-            @PathVariable("status") OrderStatus status) {
-        return orderOfTableService.getOrdersOfTablesByStatus(status);
-    }
+//
+//    @DeleteMapping
+//    public void deleteOrderOfTable(@RequestBody @Valid OrderOfTable orderOfTable) {
+//        orderOfTableService.deleteOrderOfTable(orderOfTable);
+//    }
+//
+//    @GetMapping("/{orderid}")
+//    public OrderOfTable getOrderOfTableByOrderId(@PathVariable("orderid") Long orderId) {
+//        return orderOfTableService.getOrderOfTableByOrderId(orderId);
+//    }
+//
+//    @GetMapping("/active")
+//    public List<OrderOfTable> getActiveOrdersOfTables() {
+//        return orderOfTableService.getActiveOrdersOfTables();
+//    }
+//
+//    @GetMapping("/dates/{startDate}/{endDate}")
+//    public List<OrderOfTable> getOrdersOfTablesByDates(
+//            @PathVariable("startDate") String startDate,
+//            @PathVariable("endDate") String endDate) {
+//        return OrderOfTableService.getOrdersOfTablesByDates(startDate, endDate);
+//    }
+//
+//    @GetMapping("/status/{status}")
+//    public List<OrderOfTable> getOrdersOfTablesByStatus(
+//            @PathVariable("status") OrderStatus status) {
+//        return orderOfTableService.getOrdersOfTablesByStatus(status);
+//    }
 
 
 

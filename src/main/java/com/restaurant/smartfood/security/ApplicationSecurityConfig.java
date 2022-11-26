349e03c5527135385f6c13d48a3abe3afb5f75cc
Shai <Shaisisso1@gmail.com>
2022-11-26 15:07:51 +0200

    add security configurations

--- src/main/java/com/restaurant/smartfood/security/ApplicationSecurityConfig.java
+++ src/main/java/com/restaurant/smartfood/security/ApplicationSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
